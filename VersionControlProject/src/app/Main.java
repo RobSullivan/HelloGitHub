bd2a855cb7cd70a7d41ea4388d5f1f16c942d809
RobSullivan <robertjsullivan.esq@gmail.com>
2012-10-30 20:55:27 +0000

    third commit and then push

--- VersionControlProject/src/app/Main.java
+++ VersionControlProject/src/app/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hello GitHub, this is the second commit. No?");
+		System.out.println("Hello GitHub, and a third...");
 
 	}
 
