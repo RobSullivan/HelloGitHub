7b2cfc88d6f7a41ff9dd48a5a69794620e330296
RobSullivan <robertjsullivan.esq@gmail.com>
2012-10-30 20:53:44 +0000

    Second commit before second push

--- VersionControlProject/src/app/Main.java
+++ VersionControlProject/src/app/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub, this is the second commit. No?");
 
 	}
 
